11dbe5110cfdb4cc3432c4547c0919baea6eb804
Kacper <kacpercherek1@wp.pl>
2026-01-13 17:40:59 +0100

    initiall

--- zgloszenie.php
+++ zgloszenie.php
@@ -1,8 +1,5 @@
 <?php
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
 $sukces = false; //false, bo nic nie wyslano
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") { //sprawdzenie czy sie kliknelo w wyslij 
